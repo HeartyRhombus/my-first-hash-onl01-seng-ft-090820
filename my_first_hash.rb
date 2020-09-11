957bfc1dad584e4be3fb11d8a0c2fd8f09e70dd6
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-11 19:19:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,12 +5,11 @@ end
 
 def shipping_manifest
   the_manifest = {
-    "whale bone corsets" => 5,
-    "porcelain vases" => 2,
-    "oil paintings" => 3,
-  }
-
-
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3,
+                }
+  the_manifest["oil paintings"]
 end
 
 def retrieval
@@ -20,7 +19,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+
 
 end
 
